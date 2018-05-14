557c1862029046ae027b3eb4e9beaabb034298b0
asingh <singajeet@gmail.com>
2018-05-15 02:34:23 +0530

    Added support for JAXB

---
+++ sap-design-studio-extension-support/src/com/armin/sap/ds/ext/plugin/editors/ComponentViewer.java
@@ -0,0 +1,23 @@
+package com.armin.sap.ds.ext.plugin.editors;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ComponentViewer extends ViewPart {
+
+	
+	public ComponentViewer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+	
+	}
+
+	@Override
+	public void setFocus() {
+	
+	}
+
+}
